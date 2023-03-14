a69d4352ba6fd569ff9c7ff459cb9ce4932fff96
guoji0 <83851809+guoji0@users.noreply.github.com>
2023-03-14 16:25:33 +0800

    Delete script directory

--- script/tscancode.sh
+++
@@ -1,5 +0,0 @@
-#bin/bash
-cd ~/TScancode/TscanCode/release/linux/TscanCodeV2.14.24.linux/TscanCodeV2.14.2395.linux
-echo 'tscancode安装目录为：'
-pwd
-./tscancode --xml --enable=all -q $1 > ~/$2/tscancode_`date +%Y%m%d%H%M`.xml 2>&1
